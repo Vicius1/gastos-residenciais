184904316dc1852f4f8484b2857723a98e7da5be
Vinicius <www.vinicius.oliveira@gmail.com>
2025-02-19 10:55:10 -0300

    Adicionando o endereço

--- src/server.js
+++ src/server.js
@@ -4,5 +4,5 @@ const app = new App().server;
 
 //Inicia o servidor na porta 3000
 app.listen(3000, () => {
-    console.log("Servidor rodando na porta 3000");
-})
+    console.log("Servidor rodando em: http://localhost:3000");
+});
